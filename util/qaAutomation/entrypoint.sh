7a033b1e58be815020ef0d43050b5d7d629d5ca0
Pieter Edelman <edelman@nictiz.nl>
2021-09-08 16:58:29 +0200

    Don't check for missing mappings when there's just a check on changed materials. Changed materials are not guaranteed to contain the complete mapping.

--- util/qaAutomation/entrypoint.sh
+++ util/qaAutomation/entrypoint.sh
@@ -90,7 +90,12 @@ else
   eval /scripts/generatezibsnapshots.sh $zib_profiles $zib_extensions $output_redirect
 
   if [ $? -eq 0 ]; then
-    node $tools_dir/zib-compliance-fhir/index.js -m qa/zibs2020.max -z 2020 -r -l 2 -f text --fail-at warning --zib-overrides known-issues.yml snapshots/*json
+    if [[ $changed_only == 0 ]]; then
+      check_missing="mapped-only"
+    else
+      check_missing="none"
+    fi
+    node $tools_dir/zib-compliance-fhir/index.js -m qa/zibs2020.max -z 2020 -l 2 --check-missing=$check_missing -f text --fail-at warning --zib-overrides known-issues.yml snapshots/*json
   else
     echo -e "\033[0;33mThere was an error during snapshot generation. Re-run with the --debug option to see the output.\033[0m"
     echo "Skipping zib compliance check."
