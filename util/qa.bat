f5444fcb96a699191b821cd7c4f296d8828e8f64
Pieter Edelman <edelman@nictiz.nl>
2021-10-13 13:25:55 +0200

    Add check to batch script to see if the NTS credentials file exists.
    In addition, remove outdated batch script.

--- util/qa.bat
+++ util/qa.bat
@@ -1,4 +1,9 @@
 @echo off
 
-docker-compose --env-file qaAutomation/nts-credentials.env -f qaAutomation/docker-compose.yml run --rm -p 8081:8081 qa-zib2020-r4 --menu --changed-only %*
+SET ntsCredentialsFlag=
+if exist qaAutomation/nts-credentials.env (
+  SET ntsCredentialsFlag=--env-file qaAutomation/nts-credentials.env
+)
+
+docker-compose %ntsCredentialsFlag% -f qaAutomation/docker-compose.yml run --rm -p 8081:8081 qa-zib2020-r4 --menu --changed-only %*
 pause
