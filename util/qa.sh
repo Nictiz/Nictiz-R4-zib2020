884a9c2f3f376ad07496b9476acffac1edd15343
Alexander Henket <henket@nictiz.nl>
2022-03-23 17:14:39 +0100

    Fixed setting credentials

--- util/qa.sh
+++ util/qa.sh
@@ -2,7 +2,7 @@
 
 export ntsCredentialsFlag=
 if [ -e qaAutomation/nts-credentials.env ]; then
-  export ntsCredentialsFlag=--env-file qaAutomation/nts-credentials.env
+  export ntsCredentialsFlag="--env-file qaAutomation/nts-credentials.env"
 fi
 
 docker-compose ${ntsCredentialsFlag} -f qaAutomation/docker-compose.yml run --rm -p 8081:8081 qa-zib2020-r4 --menu --changed-only $*
