0f93285499ced788a963779f8fdacc04f3bdecc3
Pieter Edelman <edelman@nictiz.nl>
2023-02-06 22:19:38 +0100

    Rewrite it in Python, which turned out to be more flexible, more precise and easier

--- util/qa/check-id.sh
+++ util/qa/check-id.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
-overall_result=0
-
-for file in $@; do
-    if [[ $file =~ \.xml$ ]]; then
-        basename=$(basename $file .xml)
-        grep -Pq "<id\s+value=['\"]$basename['\"]\s*/>" $file
-        if [[ $? != 0 ]]; then
-            overall_result=1
-            echo "The .id for $file doesn't match the filename"
-        fi
-    fi
-done
-
-exit $overall_result
+# We need this shell wrapper in order to call generatesnapshots.sh
+python3 $script_dir/check-id.py $@
